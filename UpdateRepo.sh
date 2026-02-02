c4f9bbeb5fe0ab3b58277b652448287e3c679450
Anonymous <mojang.helper.minecraft@gmail.com>
2026-02-02 12:26:36 +0100

    Initial Upload

--- UpdateRepo.sh
+++ UpdateRepo.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+dpkg-scanpackages debs /dev/null > Packages
+bzip2 -fks Packages
 
-rm Packages*
-./dpkg-scanpackages -m . /dev/null >Packages
-bzip2 Packages
+cat <<EOL > Release
+Origin: MyCydiaRepo
+Label: MyCydiaRepo
+Suite: stable
+Version: 1.0
+Codename: cydia-ios
+Architectures: iphoneos-arm
+Components: main
+Description: My personal Cydia repo
+EOL
 
+echo "Repo updated!"
